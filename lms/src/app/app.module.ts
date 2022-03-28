c4d89ce2092e808aba6a6b6359172d11dd8819d1
pronepoet <ochiengphillip84@gmail.com>
2022-03-29 00:46:07 +0300

    add a form to practice components

--- lms/src/app/app.module.ts
+++ lms/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentForm } from './content-form';
+import { FormComponent } from './form/form.component';
+import { SubmitComponent } from './submit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentForm,
+    FormComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule,
---
+++ lms/src/app/content-form.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'button1',
+    template: `
+      
+      <a href="#" class="btn btn-primary">Go somewhere</a>`
+  
+})
+export class ContentForm {
+title = "Learning Management System";
+}
